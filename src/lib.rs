4d2963c645a5f6b83d04ae86c9d5917ad1c44061
Ryan Yogan <ryanyogan@gmail.com>
2023-12-16 13:48:30 -0600

    feat // now we are coooking

---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+use edit::{edit_file, Builder};
+use slug::slugify;
+use std::{
+    fs,
+    io::{self, Write},
+    path::PathBuf,
+};
+
+pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<(), std::io::Error> {
+    let (mut file, filepath) = Builder::new()
+        .suffix(".md")
+        .rand_bytes(5)
+        .tempfile_in(&garden_path)?
+        .keep()?;
+
+    let template = format!("# {}", title.as_deref().unwrap_or(""));
+    file.write_all(template.as_bytes())?;
+    edit_file(&filepath)?;
+
+    let contents = fs::read_to_string(&filepath)?;
+
+    let document_title = title.or_else(|| {
+        contents
+            .lines()
+            .find(|v| v.starts_with("# "))
+            .map(|line| line.trim_start_matches("# ").to_string())
+    });
+
+    let filename = match document_title {
+        Some(raw_title) => confirm_filename(&raw_title),
+        None => ask_for_filename(),
+    }
+    .map(|title| slugify(title))?;
+
+    let mut dest = garden_path.join(filename);
+    dest.set_extension("md");
+    fs::rename(filepath, &dest)?;
+
+    Ok(())
+}
+
+fn ask_for_filename() -> io::Result<String> {
+    rprompt::prompt_reply(
+        "Enter filename
+> ",
+    )
+}
+
+fn confirm_filename(raw_title: &str) -> io::Result<String> {
+    loop {
+        // prompt defaults to uppercase char in question
+        // this is a convention, not a requirement enfocred by
+        // the code
+        let result = rprompt::prompt_reply(&format!(
+            "current title {}
+Do you want a different title? (y/N): ",
+            &raw_title,
+        ))?;
+
+        match result.as_str() {
+            "y" | "Y" => break ask_for_filename(),
+            "n" | "N" | "" => {
+                // the capital N in the prompt means "default",
+                // so we handle "" as input here.
+                break Ok(raw_title.to_string());
+            }
+            _ => {
+                // ask again, continue the loop...
+            }
+        };
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::path::PathBuf;
+
+use clap::{CommandFactory, Parser, Subcommand};
+use directories::UserDirs;
+
+/// A CLI for the growing and curation of a digital garden\n
+///
+/// Visit https://ryanyogan.com for more
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(short = 'p', long, env)]
+    garden_path: Option<PathBuf>,
+
+    #[clap(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// write something in your garden
+    ///
+    /// This command will open your $EDITOR, wait for you
+    /// to write something, and then save the file to your
+    /// garden.
+    Write {
+        /// Optionally set a title for what you are going to write about
+        #[clap(short, long)]
+        title: Option<String>,
+    },
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let args = Args::parse();
+
+    let Some(garden_path) = args.garden_path.or_else(get_default_garden_dir) else {
+        let mut cmd = Args::command();
+        cmd.error(
+            clap::error::ErrorKind::ValueValidation,
+            format!("garden directory not provided and home directory unreachable for default garden directory"))
+        .exit()
+    };
+
+    if !garden_path.exists() {
+        let mut cmd = Args::command();
+        cmd.error(
+            clap::error::ErrorKind::ValueValidation,
+            format!(
+                "garden directory {} does not exist, or is inaccessible",
+                garden_path.display()
+            ),
+        )
+        .exit()
+    }
+
+    match args.cmd {
+        Commands::Write { title } => garden::write(garden_path, title),
+    }
+}
+
+/// Get the user's garden directory, which by default
+/// is placed in their home directory
+fn get_default_garden_dir() -> Option<PathBuf> {
+    UserDirs::new().map(|dirs| dirs.home_dir().join("garden"))
+}
